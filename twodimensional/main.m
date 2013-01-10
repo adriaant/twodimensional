80f64b079ae926936b43069e9b57d5645d4cccd6
Adriaan Tijsseling <adriaan@tijsseling.com>
2013-01-10 20:57:50 +0100

    Initial Commit

---
+++ twodimensional/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  twodimensional
+//
+//  Created by Adriaan Tijsseling on 01/10/13.
+//  Copyright (c) 2013 Adriaan Tijsseling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
